cda2220c5e046f215fb463a233a8f92c707ae4e9
Nick <nickaroot@users.noreply.github.com>
2021-03-20 17:33:38 +0000

    ex03

--- ex03/benchmark.py
+++ ex03/benchmark.py
@@ -4,6 +4,15 @@ import timeit
 import sys
 from functools import reduce
 
+def loop(number_of_sum):
+    sum = 0
+
+    for i in range(1, number_of_sum + 1):
+        sum += i * i
+
+def r(number_of_sum):
+    reduce(lambda x, y: x + y * y, range(1, number_of_sum + 1))
+
 def main():
     if len(sys.argv) != 4:
         return
@@ -12,7 +21,17 @@ def main():
     number_of_calls = int(sys.argv[2])
     number_of_sum = int(sys.argv[3])
 
-    print(name_of_func, number_of_calls, number_of_sum)
+    func = {
+        "loop": "loop",
+        "reduce": "r"
+    }
+
+    if not (name_of_func in func):
+        return
+
+    func_time = timeit.timeit(stmt=f"{func[name_of_func]}({number_of_sum})", setup="from __main__ import loop, r", number=number_of_calls)
+
+    print(func_time)
 
 if __name__ == '__main__':
     main()
