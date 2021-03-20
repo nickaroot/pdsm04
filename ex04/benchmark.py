7f251784c87773334ac2c32aeca64dee9fa35c34
Nick <nickaroot@users.noreply.github.com>
2021-03-20 18:20:01 +0000

    ex04 fix

--- ex04/benchmark.py
+++ ex04/benchmark.py
@@ -40,17 +40,11 @@ def counter_most_common(l):
 
 def main():
     l = [random.randint(1, 100) for _ in range(1000000)]
-
-    md = my_dic(l)
-    mmc = my_most_common(l)
-
-    cd = counter_dict(l)
-    cmc = counter_most_common(l)
     
-    md_time = timeit.timeit(stmt=f"my_dic({l})", setup="from __main__ import my_dic")
-    cd_time = timeit.timeit(stmt=f"counter_dict({l})", setup="from __main__ import counter_dict")
-    mmc_time = timeit.timeit(stmt=f"my_most_common({l})", setup="from __main__ import my_most_common")
-    cmc_time = timeit.timeit(stmt=f"counter_most_common({l})", setup="from __main__ import counter_most_common")
+    md_time = timeit.timeit(stmt=f"my_dic({l})", setup="from __main__ import my_dic", number=1)
+    cd_time = timeit.timeit(stmt=f"counter_dict({l})", setup="from __main__ import counter_dict", number=1)
+    mmc_time = timeit.timeit(stmt=f"my_most_common({l})", setup="from __main__ import my_most_common", number=1)
+    cmc_time = timeit.timeit(stmt=f"counter_most_common({l})", setup="from __main__ import counter_most_common", number=1)
 
     print(f"my function: {md_time}")
     print(f"Counter: {cd_time}")
